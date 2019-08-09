7fa23c1c0a5f6d6b32b4bab87cd74df3aff14fb1
Abdul <abdul@uitouxsolutions.com>
2019-08-09 12:20:12 +0530

    updates

--- src/migrations/2019_08_06_145200_ey_addresses_c.php
+++ src/migrations/2019_08_06_145200_ey_addresses_c.php
@@ -18,8 +18,6 @@ class EyAddressesC extends Migration {
 			$table->string('name', 255);
 			$table->string('line_1', 255);
 			$table->string('line_2', 255)->nullable();
-			$table->unsignedInteger('country_id');
-			$table->unsignedInteger('state_id');
 			$table->unsignedInteger('city_id');
 			$table->string('pincode', 6);
 			$table->string('lat', 20)->nullable();
@@ -27,8 +25,6 @@ class EyAddressesC extends Migration {
 
 			$table->foreign('address_of_id')->references('id')->on('configs')->onDelete('cascade')->onUpdate('cascade');
 
-			$table->foreign('country_id')->references('id')->on('countries')->onDelete('cascade')->onUpdate('cascade');
-			$table->foreign('state_id')->references('id')->on('nstates')->onDelete('cascade')->onUpdate('cascade');
 			$table->foreign('city_id')->references('id')->on('ncities')->onDelete('cascade')->onUpdate('cascade');
 		});
 	}
---
+++ src/migrations/2019_08_09_115220_ey_employee_u1.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class EyEmployeeU1 extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::table('employees', function (Blueprint $table) {
+			$table->string('name', 80)->after('code');
+
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::table('employees', function (Blueprint $table) {
+			$table->dropColumn('name');
+		});
+
+	}
+}
