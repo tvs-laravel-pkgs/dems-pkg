d618996996eeb6099e259a09e22d42bd732f3007
Ramakrishnan <45380286+ramuitoux@users.noreply.github.com>
2019-08-24 19:07:59 +0530

    employee trip claim changes inprogress

---
+++ src/models/Employeeclaim.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Uitoux\EYatra;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class EmployeeClaim extends Model {
+	use SoftDeletes;
+	protected $table = 'ey_employee_claims';
+
+	protected $fillable = [
+		'employee_id',
+		'trip_id',
+		'total_amount',
+		'status_id',
+		'payment_id',
+	];
+}
