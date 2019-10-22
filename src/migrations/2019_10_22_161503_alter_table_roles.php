edc1f9086e634386e8ae422c91404e28fa8cbf74
Parthi <parthiban@uitouxsolutions.com>
2019-10-22 16:22:43 +0530

    Roles Migration

---
+++ src/migrations/2019_10_22_161503_alter_table_roles.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterTableRoles extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::table('roles', function (Blueprint $table) {
+			$table->unsignedInteger('company_id')->default(1)->after('id');
+			$table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade')->onUpdate('cascade');
+
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		//
+	}
+}
