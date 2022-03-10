5ac801c58a292d6d67997e1810949684bd630c01
rajarajan-uitoux <82135061+rajarajan-uitoux@users.noreply.github.com>
2022-03-10 17:58:33 +0530

    Update EntitiesSeeder.php

--- src/database/seeds/EntitiesSeeder.php
+++ src/database/seeds/EntitiesSeeder.php
@@ -87,9 +87,9 @@ class EntitiesSeeder extends Seeder {
 						continue;
 					}
 
-					$existing_entities = Entity::where('entity_type_id',$entity_type->id)->where('name',$val->name)
+					$existing_entities = Entity::select('id')->where('entity_type_id',$entity_type->id)->where('name',$val->name)
 					->where('display_order',$val->display_order)->where('company_id',$company->id)->first();
-					dump('existing_entities',$existing_entities);
+					
 					if (!$existing_entities) {
 						dump('Record No: ' . ($key + 1) . ' - Entity Already Exist');
 						continue;
