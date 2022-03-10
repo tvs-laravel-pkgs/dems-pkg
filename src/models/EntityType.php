2fb1104559402b46201ecf9ca266419e65612eae
rajarajan-uitoux <82135061+rajarajan-uitoux@users.noreply.github.com>
2022-03-10 17:38:43 +0530

    Update EntityType.php

--- src/models/EntityType.php
+++ src/models/EntityType.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class EntityType extends Model {
-	use SoftDeletes;
 	public $timestamps = false;
 	protected $fillable = [
 		'id',
