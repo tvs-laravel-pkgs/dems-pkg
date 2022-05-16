52a78e3034d280dcfdc1594e19aa409a0478b3cd
sagayamary-uitoux <sagayamary@uitoux.in>
2022-05-16 17:05:17 +0530

    date grater than changes

--- src/LocalTripController.php
+++ src/LocalTripController.php
@@ -248,8 +248,10 @@ class LocalTripController extends Controller {
 		if ($trip_start_date_data) {
 			return response()->json(['success' => false, 'errors' => "You have another local trip on this trip period"]);
 		}
-		$date_lessthan_previous_trip = LocalTrip::where('employee_id', Auth::user()->entity_id)
+		$date_lessthan_previous_trip = LocalTrip::select('id')->where('employee_id', Auth::user()->entity_id)
+		        ->where('id','!=',$request->id)
 				->where('end_date', '>=', date("Y-m-d", strtotime($request->start_date)))
+				->where('status_id','!=',3032)
 				->first();
 		if($date_lessthan_previous_trip){
 		 return response()->json(['success' => false, 'errors' => "Trip date should be Greater than your previous trip"]);
