124d1cb015a0639aa69269237026480efcd5e79a
Ram <45380286+ram-uitoux@users.noreply.github.com>
2022-08-16 10:57:39 +0530

    Update nstateSeeder.php

--- src/database/seeds/nstateSeeder.php
+++ src/database/seeds/nstateSeeder.php
@@ -1,107 +1,123 @@
 <?php
 
 namespace Uitoux\EYatra\Database\Seeds;
-use PHPExcel_IOFactory;
-use Validator;
-use Uitoux\EYatra\NState;
-use Uitoux\EYatra\NCountry;
+
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
+use PHPExcel_IOFactory;
+use Uitoux\EYatra\NCountry;
+use Uitoux\EYatra\NState;
 
-class nstateSeeder extends Seeder
-{
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        $objPHPExcel = PHPExcel_IOFactory::load('public/excel-imports/nstates.xlsx');
-        $sheet = $objPHPExcel->getSheet(0);
-        $highestRow = $sheet->getHighestDataRow();
-        $highestColumn = $sheet->getHighestDataColumn();
-        $header = $sheet->rangeToArray('A1:'.$highestColumn. '1',null,true,false);
-        $header = $header[0];
-        foreach ($header as $key => $column) {
-            if ($column == null) {
-                unset($header[$key]);
-            }
-        }
+class NstateSeeder extends Seeder {
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run() {
 
-        $nstates = $sheet->rangeToArray('A2:'.$highestColumn . $highestRow, null, true, false);
-        if(!empty($nstates)) {
-            foreach ($nstates as $key => $nstateValues) {
-                $val = [];
-                foreach ($header as $headerKey => $column) {
-                    if (!$column) {
-                        continue;
-                    } else {
-                        $header_col = str_replace(' ', '_', strtolower($column));
-                        $val[$header_col] = $nstateValues[$headerKey];
-                    }
-                }
-                $val = (object)$val;
-                try {
-                    if (empty($val->country)) {
-                        dump('Record No: ' . ($key + 1) . ' - country is required');
-						continue;
-                    }if (empty($val->code)) {
-                        dump('Record No: ' . ($key + 1) . ' - code is required');
-						continue;
-                    }
-                    if (empty($val->name)) {
-                        dump('Record No: ' . ($key + 1) . ' - name is required');
-						continue;
-                    }
-                    dump($val->country,$val->code,$val->name);
+		$objPHPExcel = PHPExcel_IOFactory::load('public/excel-imports/nstates.xlsx');
+		$sheet = $objPHPExcel->getSheet(0);
+		$highestRow = $sheet->getHighestDataRow();
+		$highestColumn = $sheet->getHighestDataColumn();
+		$header = $sheet->rangeToArray('A1:' . $highestColumn . '1', null, true, false);
+		$header = $header[0];
+		foreach ($header as $key => $column) {
+			if ($column == null) {
+				unset($header[$key]);
+			}
+		}
 
-                    $validator = Validator::make((array) $val, [
-                        'code' => [
-                            'string',
-                            'max:2',
-                        ],
-                        'name' => [
-                            'string',
-                            'max:191',
-                        ]
-                    ]);
-                    if ($validator->fails()) {
-						dump('Record No: ' . ($key + 1) . ' ' . implode('', $validator->errors()->all()));
+		$nstates = $sheet->rangeToArray('A2:' . $highestColumn . $highestRow, null, true, false);
+		if (!empty($nstates)) {
+			foreach ($nstates as $key => $nstateValue) {
+				$val = [];
+				foreach ($header as $headerKey => $column) {
+					if (!$column) {
+						continue;
+					} else {
+						$header_col = str_replace(' ', '_', strtolower($column));
+						$val[$header_col] = $nstateValue[$headerKey];
+					}
+				}
+				$val = (object) $val;
+				try {
+					if (empty($val->country)) {
+						dump('Record No: ' . ($key + 1) . ' - Country is required');
 						continue;
 					}
-                    dump($val->country,$val->code,$val->name);
 
-                    $country = NCountry::select('id')->where('name',$val->country)->first();
-
-                    if(!$country && !$country->id){
-                        dump('Record No: ' . ($key + 1) . ' - Country Not Found');
+					if (empty($val->code)) {
+						dump('Record No: ' . ($key + 1) . ' - Code is required');
 						continue;
-                    }
+					}
 
-                    $exist_nstate = NState::where('country_id',$country->id)
-                    ->where('code', $val->code)
-                    ->where('name', $val->name)
-                    ->first();
-
-                    if ($exist_nstate) {
-                        dump('Record No: ' . ($key + 1) . ' - NState is ALready Exist');
+					if (empty($val->name)) {
+						dump('Record No: ' . ($key + 1) . ' - Name is required');
 						continue;
-                    }
+					}
 
-                    $new_nstate = new NState;
-                    $new_nstate->country_id = $country->id;
-                    $new_nstate->code = $val->code;
-                    $new_nstate->name = $val->name;
-                    $new_nstate->created_by = 1;
-                    $new_nstate->save();
-                    dump(' === updated === ');
+					if (empty($val->gstin_state_code)) {
+						dump('Record No: ' . ($key + 1) . ' - GSTIN state code is required');
+						continue;
+					}
 
-                } catch (\Exception $e) {
+					if (empty($val->axapta_cgst_code)) {
+						dump('Record No: ' . ($key + 1) . ' - Axapta CGST code is required');
+						continue;
+					}
+
+					if (empty($val->axapta_sgst_code)) {
+						dump('Record No: ' . ($key + 1) . ' - Axapta SGST code is required');
+						continue;
+					}
+
+					if (empty($val->axapta_igst_code)) {
+						dump('Record No: ' . ($key + 1) . ' - Axapta IGST code is required');
+						continue;
+					}
+
+					dump($val->country, $val->code, $val->name);
+
+					$country = NCountry::select([
+						'id',
+					])
+						->where('name', $val->country)
+						->first();
+
+					if (!$country) {
+						dump('Record No: ' . ($key + 1) . ' - Country not found');
+						continue;
+					}
+
+					$nstate = NState::firstOrNew([
+						'country_id' => $country->id,
+						'code' => $val->code,
+						'name' => $val->name,
+					]);
+
+					if ($nstate->exists) {
+						$nstate->updated_at = Carbon::now();
+						$nstate->updated_by = 1;
+					} else {
+						$nstate->created_at = Carbon::now();
+						$nstate->created_by = 1;
+					}
+
+					$nstate->gstin_state_code = $val->gstin_state_code;
+					$nstate->axapta_cgst_code = $val->axapta_cgst_code;
+					$nstate->axapta_sgst_code = $val->axapta_sgst_code;
+					$nstate->axapta_igst_code = $val->axapta_igst_code;
+					$nstate->save();
+
+					dump(' === success === ');
+
+				} catch (\Exception $e) {
 					dump($e);
 					continue;
 				}
-            }
-            dd(' == completed ==');
-        }
-    }
+			}
+		}
+		dd(' == completed ==');
+	}
 }
