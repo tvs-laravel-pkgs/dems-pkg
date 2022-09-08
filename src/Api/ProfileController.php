c239c4ad8f5989c960b6c453eb5f5f82f7b6ee8a
sagayamary-uitoux <sagayamary@uitoux.in>
2022-09-08 12:15:05 +0530

    vehicle details api changes

--- src/Api/ProfileController.php
+++ src/Api/ProfileController.php
@@ -36,21 +36,51 @@ class ProfileController extends Controller {
 		return app('App\Http\Controllers\AngularController')->verifyVehicleDtails($r->registration_number);
 	}
 	public function saveVehicleDetails(Request $request) {
+		//dd($request->all());
+		try {
+			$error_messages = [
+				'registration_number.required' => "Registration Number is Required",
+				'registration_number.unique' => "Registration Number is already taken",
+				'vehicle_attachment.required' => "Vehicle Attachment is Required",
+				'kilometer_attachment.required' => "Kilometer Attachment is Required",
+				
+			];
+
+			$validator = Validator::make($request->all(), [
+				'registration_number' => [
+					'required:true',
+					'unique:vehicle_details,registration_number,' . $request->registration_number . ',id',
+                ],
+                'vehicle_attachment' => 'required|mimes:jpeg,jpg|max:1024',
+				'kilometer_attachment' => 'required|mimes:jpeg,jpg|max:1024',
+			], $error_messages);
+			if ($validator->fails()) {
+				return response()->json(['success' => false, 'errors' => $validator->errors()->all()]);
+			}
 		$user = User::where('id', $request->user_id)->first();
 		$employee = Employee::where('code',$user->username)->first();
+		$type=VehicleDetails::select('vehicle_type','id')->get()->toArray();
+			if(count($type) == 2){
+				return response()->json(['success' => false, 'errors' => ['The vehicle max limit is two']]); 
+			}
 		if ($user && $employee) {
-            $employee->registration_number=$request->registration_number;
-            $employee->vehicle_user_name=$request->vehicle_user_name;
-            $employee->current_km=$request->current_km;
-            $employee->insurance_company=$request->insurance_company;
-            $employee->insurance_expiry_date=date("Y-m-d", strtotime($request->insurance_expiry_date));
-            $employee->insurance_policy_number=$request->insurance_policy_number;
-            $employee->rc_chassis_number=$request->rc_chassis_number;
-            $employee->vehicle_make_model=$request->vehicle_make_model;
-            $employee->vehicle_type=$request->vehicle_type;
-            $employee->vehicle_fuel_description=$request->vehicle_fuel_description;
-            $employee->vehicle_maker_description=$request->vehicle_maker_description;
-            $employee->save();
+			DB::beginTransaction();
+			if (!$request->id) {
+				$vehicle_details = new VehicleDetails;
+				$vehicle_details->created_at = Carbon::now();
+				$vehicle_details->updated_at = NULL;
+			} else {
+				$vehicle_details = VehicleDetails::withTrashed()->find($request->id);
+				$vehicle_details->updated_at = Carbon::now();
+			}
+			$vehicle_details->employee_id=$employee->id;
+			$vehicle_details->fill($request->all());
+            $vehicle_details->insurance_expiry_date=date("Y-m-d", strtotime($request->insurance_expiry_date));
+            $vehicle_details->vehicle_make_model = $request->vehicle_make_model;
+            $vehicle_details->vehicle_type = $request->vehicle_type;
+            $vehicle_details->vehicle_fuel_description = $request->vehicle_fuel_description;
+            $vehicle_details->vehicle_maker_description = $request->vehicle_maker_description;
+            $vehicle_details->save();
 
 			$vehicle_images = storage_path('app/public/vehicle/');
 			Storage::makeDirectory($vehicle_images, 0777);
@@ -58,11 +88,11 @@ class ProfileController extends Controller {
 				$value = rand(1, 100);
 				$image = $request->vehicle_attachment;
 				$extension = $image->getClientOriginalExtension();
-				$name = $user->id . 'vehicle_attachment' . $value . '.' . $extension;
+				$name = $vehicle_details->id . 'vehicle_attachment' . $value . '.' . $extension;
 				$des_path = storage_path('app/public/vehicle/');
 				$image->move($des_path, $name);
-				$employee->vehicle_image = $name;
-				$employee->save();
+				$vehicle_details->vehicle_image = $name;
+				$vehicle_details->save();
 			}
 
 				$kilometer_images = storage_path('app/public/vehicle/');
@@ -71,18 +101,23 @@ class ProfileController extends Controller {
 				$value = rand(1, 100);
 				$image = $request->kilometer_attachment;
 				$extension = $image->getClientOriginalExtension();
-				$name = $user->id . 'kilometer_attachment' . $value . '.' . $extension;
+				$name = $vehicle_details->id . 'kilometer_attachment' . $value . '.' . $extension;
 				$des_path = storage_path('app/public/vehicle/');
 				$image->move($des_path, $name);
-                $employee->kilometer_image = $name;
-				$employee->save();
+                $vehicle_details->kilometer_image = $name;
+				$vehicle_details->save();
 			}
+			DB::commit();
 
 				// $path = url('') . '/storage/app/public/profile/' . $name;
 			return response()->json(['success' => true, 'message' => 'Vehicle Details saved successfully!', 'employee' => $employee]);
 			
 		} else {
 			return response()->json(['success' => false, 'message' => 'User Not Found!']);
+		}
+		} catch (Exception $e) {
+			DB::rollBack();
+			return response()->json(['success' => false, 'errors' => ['Exception Error' => $e->getMessage()]]);
 		}
 	}
 }
