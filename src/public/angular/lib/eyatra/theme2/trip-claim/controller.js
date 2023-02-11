052e9b95b202d9204f83a13ad6c2564957867071
Karthick <karthick.r@uitoux.in>
2023-02-11 14:18:09 +0530

    grade amount validation

--- src/public/angular/lib/eyatra/theme2/trip-claim/controller.js
+++ src/public/angular/lib/eyatra/theme2/trip-claim/controller.js
@@ -1615,14 +1615,27 @@ app.component('eyatraTripClaimForm', {
                     } else {
                         default_eligible_amount = parseInt(default_eligible_amount);
                     }
-                    if (amount_entered > default_eligible_amount) {
-                        is_deviation = true;
+                    // if (amount_entered > default_eligible_amount) {
+                    //     is_deviation = true;
 
-                        var isDeviationType = $(this).closest('.is_deviation_amount_row').find('.deviation_type').val()
+                    //     var isDeviationType = $(this).closest('.is_deviation_amount_row').find('.deviation_type').val()
 
-                        if (jQuery.inArray(isDeviationType, deviationTypes) == -1)
-                            deviationTypes[deviationTypes.length] = isDeviationType;
+                    //     if (jQuery.inArray(isDeviationType, deviationTypes) == -1)
+                    //         deviationTypes[deviationTypes.length] = isDeviationType;
+                    // }
+
+
+                    if(self.employee.grade && $.inArray(self.employee.grade, ['L1','L2','L3','L4','L5','L6','L7','L8','L9']) == -1){
+                        if (amount_entered > default_eligible_amount) {
+                            is_deviation = true;
+
+                            var isDeviationType = $(this).closest('.is_deviation_amount_row').find('.deviation_type').val()
+
+                            if (jQuery.inArray(isDeviationType, deviationTypes) == -1)
+                                deviationTypes[deviationTypes.length] = isDeviationType;
+                        }
                     }
+
                 });
             }
             console.log({ deviationTypes })
