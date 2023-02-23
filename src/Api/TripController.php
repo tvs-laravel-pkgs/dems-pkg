0e606b94bed5f1b8bf72496b9952b4681bcf41c6
Karthick-T <karthick.t@uitoux.in>
2023-02-23 18:40:36 +0530

    revert api validation

--- src/Api/TripController.php
+++ src/Api/TripController.php
@@ -74,7 +74,7 @@ class TripController extends Controller {
 				->where('status_id','!=',3032)
 				->first();
 		if($date_lessthan_previous_trip){
-		//  return response()->json(['success' => false, 'errors' => ["Trip date should be Greater than your previous trip"]]);
+		 return response()->json(['success' => false, 'errors' => ["Trip date should be Greater than your previous trip"]]);
 		}
 		// dd($request->visits);
 		$size = sizeof($request->visits);
