eb2183d0051384260334280e72633359237457e0
sivakumar-uitoux <83207179+sivakumar-uitoux@users.noreply.github.com>
2024-05-22 16:16:00 +0530

    remainder mail changes

--- src/MailController.php
+++ src/MailController.php
@@ -11,78 +11,77 @@ class MailController extends Controller
 {
     // Send mail
     public function sendMail() {
-        
         $status='Pending Requsation Approval';
         //2- NORMAL
         $date = date('Y-m-d', strtotime('-3days'));
         $title='Remainder';
-        $local_trip_mail = LocalTrip::pendingTripMail($date,$status,$title);
+        //$local_trip_mail = LocalTrip::pendingTripMail($date,$status,$title);
         $trip_mail = Trip::pendingTripMail($date,$status,$title);
         
         // 8-WARNING
         $date = date('Y-m-d', strtotime('-9days'));
         $title='Warning';
-        $local_trip_mail = LocalTrip::pendingTripMail($date,$status,$title);
+        //$local_trip_mail = LocalTrip::pendingTripMail($date,$status,$title);
         $trip_mail = Trip::pendingTripMail($date,$status,$title);
         
         //10-CANCELLATION
-        $date = date('Y-m-d', strtotime('-11days'));
-        $title='Cancelled';
-        $local_trip_mail = LocalTrip::pendingTripMail($date,$status,$title);
-        $trip_mail = Trip::pendingTripMail($date,$status,$title);
+        //$date = date('Y-m-d', strtotime('-11days'));
+        //$title='Cancelled';
+        //$local_trip_mail = LocalTrip::pendingTripMail($date,$status,$title);
+        //$trip_mail = Trip::pendingTripMail($date,$status,$title);
         
         $status='Claim Generation';
         //2- NORMAL
         $date = date('Y-m-d', strtotime('-3days'));
         $title='Remainder';
-        $local_trip_mail = LocalTrip::pendingTripMail($date,$status,$title);
+        //$local_trip_mail = LocalTrip::pendingTripMail($date,$status,$title);
         $trip_mail = Trip::pendingTripMail($date,$status,$title);
          // 8-WARNING
         $date = date('Y-m-d', strtotime('-13days'));
         $title='Warning';
-        $local_trip_mail = LocalTrip::pendingTripMail($date,$status,$title);
+        //$local_trip_mail = LocalTrip::pendingTripMail($date,$status,$title);
         $trip_mail = Trip::pendingTripMail($date,$status,$title);
         
         //10-CANCELLATION
-        $date = date('Y-m-d', strtotime('-16days'));
-        $title='Cancelled';
-        $local_trip_mail = LocalTrip::pendingTripMail($date,$status,$title);
-        $trip_mail = Trip::pendingTripMail($date,$status,$title);
+        //$date = date('Y-m-d', strtotime('-16days'));
+        //$title='Cancelled';
+        //$local_trip_mail = LocalTrip::pendingTripMail($date,$status,$title);
+        //$trip_mail = Trip::pendingTripMail($date,$status,$title);
         
         $status='Pending Claim Approval';
         //2- NORMAL
         $date = date('Y-m-d', strtotime('-3days'));
         $title='Remainder';
-        $local_trip_mail = LocalTrip::pendingTripMail($date,$status,$title);
+        //$local_trip_mail = LocalTrip::pendingTripMail($date,$status,$title);
         $trip_mail = Trip::pendingTripMail($date,$status,$title);
         // 8-WARNING
         $date = date('Y-m-d', strtotime('-9days'));
         $title='Warning';
-        $local_trip_mail = LocalTrip::pendingTripMail($date,$status,$title);
+        //$local_trip_mail = LocalTrip::pendingTripMail($date,$status,$title);
         $trip_mail = Trip::pendingTripMail($date,$status,$title);
         //10-CANCELLATION
-        $date = date('Y-m-d', strtotime('-11days'));
-        $title='Cancelled';
-        $local_trip_mail = LocalTrip::pendingTripMail($date,$status,$title);
-        $trip_mail = Trip::pendingTripMail($date,$status,$title);
+        //$date = date('Y-m-d', strtotime('-11days'));
+        //$title='Cancelled';
+        //$local_trip_mail = LocalTrip::pendingTripMail($date,$status,$title);
+        //$trip_mail = Trip::pendingTripMail($date,$status,$title);
 
         $status='Pending Divation Claim Approval';
         //2- NORMAL
         $date = date('Y-m-d', strtotime('-3days'));
         $title='Remainder';
-        $local_trip_mail = LocalTrip::pendingTripMail($date,$status,$title);
+        //$local_trip_mail = LocalTrip::pendingTripMail($date,$status,$title);
         $trip_mail = Trip::pendingTripMail($date,$status,$title);
         
         // 5-WARNING
         $date = date('Y-m-d', strtotime('-6days'));
         $title='Warning';
-        $local_trip_mail = LocalTrip::pendingTripMail($date,$status,$title);
+        //$local_trip_mail = LocalTrip::pendingTripMail($date,$status,$title);
         $trip_mail = Trip::pendingTripMail($date,$status,$title);
         //10-CANCELLATION
-        $date = date('Y-m-d', strtotime('-11days'));
-        $title='Cancelled';
-        $local_trip_mail = LocalTrip::pendingTripMail($date,$status,$title);
-        $trip_mail = Trip::pendingTripMail($date,$status,$title);
+        //$date = date('Y-m-d', strtotime('-11days'));
+        //$title='Cancelled';
+        //$local_trip_mail = LocalTrip::pendingTripMail($date,$status,$title);
+        //$trip_mail = Trip::pendingTripMail($date,$status,$title);
         return true;
     }
 }
